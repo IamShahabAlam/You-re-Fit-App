3ce171c3d4b90fd8642543234ad7452ec02d90e0
Shahab Alam <iamshahabalam@gmail.com>
2022-06-10 23:25:20 +0500

    Initial Commit

--- lib/screens/bmi_Result.dart
+++ lib/screens/bmi_Result.dart
@@ -64,6 +64,7 @@ BMI_Result_Page({this.bmiResult,this.resultText,this.suggestion});
               width: _w*0.8,
               decoration: BoxDecoration(
                 color: Colors.orange[900],
+                boxShadow: [kBtnBoxShadow]
               ),
               child: Text(suggestion,style: kBMIsuggest, textAlign: TextAlign.center,),
             ),
