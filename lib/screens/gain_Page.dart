e947af5d2be66363f9cf206c592df1c2b5bbd1d6
Shahab Alam <iamshahabalam@gmail.com>
2022-06-16 12:54:14 +0500

    Initial Commit

---
+++ lib/screens/gain_Page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Weight_Gain extends StatefulWidget {
+
+  @override
+  _Weight_GainState createState() => _Weight_GainState();
+}
+
+class _Weight_GainState extends State<Weight_Gain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/loss_Page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Weight_Loss extends StatefulWidget {
+
+  @override
+  _Weight_LossState createState() => _Weight_LossState();
+}
+
+class _Weight_LossState extends State<Weight_Loss> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
