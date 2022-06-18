da0943b6eeb644ab9e616ccd59364e5eb5cb08bb
Shahab Alam <iamshahabalam@gmail.com>
2022-06-18 23:04:23 +0500

    Initial Commit

--- lib/screens/gain_Page.dart
+++ lib/screens/gain_Page.dart
@@ -1,14 +1,107 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youre_fit/components/buttons.dart';
+import 'package:youre_fit/components/conatiners.dart';
+import 'package:youre_fit/components/constants.dart';
+import 'package:youre_fit/screens/bmr_Result.dart';
+import 'package:clippy_flutter/clippy_flutter.dart';
+import 'package:youre_fit/components/bmr_brain.dart';
+import 'package:youre_fit/screens/home.dart';
 
-class Weight_Gain extends StatefulWidget {
 
-  @override
-  _Weight_GainState createState() => _Weight_GainState();
-}
+class Weight_Gain extends StatelessWidget {
+  Weight_Gain({this.SedVal,this.MWG , this.WG , this.FWG});
 
-class _Weight_GainState extends State<Weight_Gain> {
+  final String SedVal, MWG , WG , FWG;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    final _h = MediaQuery.of(context).size.height  ;
+    final _w = MediaQuery.of(context).size.width;
+
+    return Scaffold( backgroundColor: kBG,
+      appBar: AppBar( centerTitle: true, backgroundColor: kinactiveCardColor, elevation: 15.0, shadowColor: kactiveCardColor,
+        title: Text("Weight Gain Guide" , style: kweightGuide,),
+        leading: AppbarLead(color: kactiveCardColor , page: BMR_Result_Page(SVal: SedVal,), icon: Icons.arrow_back_ios,),
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Container( margin: EdgeInsets.all(15) ,
+            //     alignment: Alignment.topLeft,
+            //     child: Text("Weight Loss Guide",style: kweightGuide,)),
+
+            Container(
+                padding: EdgeInsets.only(right: 15, left : 15,bottom: 35, top: 25.0),
+                child: Text("🔶  The results show a number of daily calorie estimates that can be used as a guideline for how many calories to consume each day to maintain, gain weight at a chosen rate.",
+                  textAlign: TextAlign.justify , style: TextStyle(fontSize: 16,),)),
+
+            Column( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Guide_List(
+                  type: "Maintain Weight",
+                  kgPerWeek: "0",
+                  calories: SedVal ,
+                ),
+
+                Guide_List(
+                  type: "Mild Weight Gain",
+                  kgPerWeek: "0.15",
+                  calories: MWG,
+                ),
+
+                Guide_List(
+                  type: "Weight Gain",
+                  kgPerWeek: "0.3",
+                  calories: WG,
+                ),
+
+                Guide_List(
+                  type: "Fast Gain",
+                  kgPerWeek: "0.5",
+                  calories: FWG,
+                ),
+
+                // Container(
+                //     padding: EdgeInsets.only(right: 15, left : 15,bottom: 15.0, top: 15.0),
+                //     child: Text("🔶  Please consult with a doctor when losing 0.5 kg or more per week consuming less than  1,200 calories a day may harm you.",
+                //       textAlign: TextAlign.justify , style: TextStyle(fontSize: 16,),)),
+
+                Row( mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+                  children: [
+                    HomeBtn(heightB: 0.07, widthB: 0.4 ,
+                      t_margin: 0.08,
+                      name: "Home",
+                      onpressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Home(), ));
+                      },
+
+                      firstClr: kactiveCardColor ,
+                      lastClr : Colors.blueGrey[800],
+                    ),
+
+                  ],),
+
+
+
+              ],
+            ),
+          ],
+        ),
+      )  ,
+    );
   }
 }
+
+
+
+
+
+
+
+
+
